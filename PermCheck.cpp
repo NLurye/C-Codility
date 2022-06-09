873382b4b430a2201c6d8e80a2a6cb3cf1c781f9
NLurye <103998165+NLurye@users.noreply.github.com>
2022-06-09 23:54:30 +0300

    Create PermCheck.cpp

---
+++ PermCheck.cpp
@@ -0,0 +1,59 @@
+/*A non-empty array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+is a permutation, but array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+the function should return 1.
+
+Given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [1..1,000,000,000].*/
+
+#include <algorithm>
+#include <set>
+using namespace std;
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+int size = A.size();
+set<int> S;
+for(auto& n : A)
+S.insert(n);
+return (S.size()==size)&&(*S.rbegin()==size)? 1:0;//all numbers appeared in set and no one is missing
+}
